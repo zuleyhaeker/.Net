ec464b51b6ffc93a26fc71c7038430f2c45cbe6e
zuleyhaeker <60626948+zuleyhaeker@users.noreply.github.com>
2022-03-30 11:19:49 +0300

    Add files via upload

---
+++ operatorler/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace operatorler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+               int x = 3;
+            int y = 3;
+
+            y = y + 2;
+            Console.WriteLine(y);
+
+            y += 2;
+            Console.WriteLine(y);
+
+            y /= 1;
+            Console.WriteLine(y);
+
+            x *= 2;
+            Console.WriteLine(x);
+
+            // Logical Operators
+            // ||, &&, !
+
+            bool isSuccess = true;
+            bool isCompleted = false;
+
+            if (isSuccess && isCompleted)
+            {
+                Console.WriteLine("Perfect!");
+            }
+            else if (isSuccess || isCompleted)
+            {
+                Console.WriteLine("Great!");
+            }
+            else if (isSuccess && !isCompleted)
+            {
+                Console.WriteLine("Fine!");
+            }
+
+            // Comparison Operators
+            // <, >, <=, >=, ==, !=
+
+            int a = 3;
+            int b = 4;
+            bool result = a < b;
+            Console.WriteLine(result);
+
+            result = a > b;
+            Console.WriteLine(result);
+
+            result = a >= b;
+            Console.WriteLine(result);
+
+            result = a <= b;
+            Console.WriteLine(result);
+
+            result = a == b;
+            Console.WriteLine(result);
+
+            result = a != b;
+            Console.WriteLine(result);
+
+            // Arithmetic Operators
+            // /, *, +, -
+
+            int num1 = 10;
+            int num2 = 5;
+
+            int result1 = num1 / num2;
+            Console.WriteLine(result1);
+
+            int result2 = num1 * num2;
+            Console.WriteLine(result2);
+
+            int result3 = num1 + num2;
+            Console.WriteLine(result3);
+
+            int result4 = num1++;
+            Console.WriteLine(result4);
+
+            // % -> Modulo
+            int result5 = 20 % 3;
+            Console.WriteLine(result5);
+        }
+    }
+}
